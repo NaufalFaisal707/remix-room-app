3ff2026bf7c8c716d70707dc96390f3bef7dd22a
NaufalFaisal707 <naufalfaisal707@gmail.com>
2025-01-10 09:54:49 +0700

    menambahkan nama aplikasi

--- app/root.tsx
+++ app/root.tsx
@@ -32,6 +32,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Real-time chat application with socket.io and Remix"
+        />
+        <title>Room</title>
         <Meta />
         <Links />
       </head>
